2589215b957073de20a1adeef35d9bc08cbbff45
SantewareAakash <aakash@santeware.com>
2023-06-15 17:48:15 +0530

    initialized and homepage created

---
+++ src/components/layout/shared/NavBar.js
@@ -0,0 +1,38 @@
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "../StyleSheets/Navbar.css";
+
+const NavBar = () => {
+
+
+  return (
+    <Navbar expand="lg" STYLE={{ padding: 0 }}>
+      <Container>
+        <Navbar.Brand href="#">
+          <img
+            src="/assets/images/logo.png"
+            className="img-fluid navBarLogo"
+            alt="logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="ms-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <div className="buttonContainer d-flex gap-2">
+              <a className="buttonEarlyAccess" href="#">Get Early Access</a>
+              {/* <a className="buttonJoinUs" href="mailto:classhealth500@gmail.com">Join Us</a> */}
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
