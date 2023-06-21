92a4c2f19f5b441baa9f1bf4d7291940fa975de3
santewareaakash <aakash@santeware.com>
2023-06-21 14:46:10 +0530

    privacy policy page content update

--- src/pages/PrivacyPolicy/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy/PrivacyPolicy.js
@@ -18,36 +18,242 @@ const PrivacyPolicy = () => {
           <Row className="align-items-center justify-content-center">
             <Col md={10}>
               <div className="content-1">
-                <h4>Lorem Ipsum</h4>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Corporis eius nesciunt et tempora accusamus, eligendi
-                  similique harum expedita cumque magni officiis ipsum iusto hic
-                  deleniti temporibus ab aliquam maiores laudantium? Quisquam
-                  blanditiis aliquam quam voluptatibus fuga voluptates ad
-                  molestiae labore, numquam sapiente cumque saepe ducimus odio
-                  vel quos ex non?
+                  ClassHealth Technologies Private Limited ("us", "we", or
+                  "ClassHealth", which also includes its affiliates) is the
+                  author and publisher of the internet resource
+                  www.classhealth.in ("Website") on the world wide web as well
+                  as the software and applications provided by ClassHealth,
+                  including but not limited to the mobile application
+                  'ClassHealth, and the software and applications of the brand
+                  names 'ClassHealth', and 'Health Account' (together with the
+                  Website, referred to as the "Services").
                 </p>
-                <h4>Lorem Ipsum</h4>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Corporis eius nesciunt et tempora accusamus, eligendi
-                  similique harum expedita cumque magni officiis ipsum iusto hic
-                  deleniti temporibus ab aliquam maiores laudantium? Quisquam
-                  blanditiis aliquam quam voluptatibus fuga voluptates ad
-                  molestiae labore, numquam sapiente cumque saepe ducimus odio
-                  vel quos ex non?
+                  This privacy policy ("Privacy Policy") explains how we
+                  collect, use, share, disclose and protect Personal information
+                  about the Users of the Services, including the Practitioners
+                  (as defined in the Terms of Use, which may be accessed via the
+                  following weblink https://classhealth.in/privacy (the "Terms
+                  of Use")), the End-Users (as defined in the Terms of Use), and
+                  the visitors of Website (jointly and severally referred to as
+                  "you" or "Users" in this Privacy Policy). We created this
+                  Privacy Policy to demonstrate our commitment to the protection
+                  of your privacy and your personal information. Your use of and
+                  access to the Services is subject to this Privacy Policy and
+                  our Terms of Use. Any capitalized term used but not defined in
+                  this Privacy Policy shall have the meaning attributed to it in
+                  our Terms of Use. BY USING THE SERVICES OR BY OTHERWISE GIVING
+                  US YOUR INFORMATION, YOU WILL BE DEEMED TO HAVE READ,
+                  UNDERSTOOD AND AGREED TO THE PRACTICES AND POLICIES OUTLINED
+                  IN THIS PRIVACY POLICY AND AGREE TO BE BOUND BY THE PRIVACY
+                  POLICY. YOU HEREBY CONSENT TO OUR COLLECTION, USE AND SHARING,
+                  DISCLOSURE OF YOUR INFORMATION AS DESCRIBED IN THIS PRIVACY
+                  POLICY. WE RESERVE THE RIGHT TO CHANGE, MODIFY, ADD OR DELETE
+                  PORTIONS OF THE TERMS OF THIS PRIVACY POLICY, AT OUR SOLE
+                  DISCRETION, AT ANY TIME. IF YOU DO NOT AGREE WITH THIS PRIVACY
+                  POLICY AT ANY TIME, DO NOT USE ANY OF THE SERVICES OR GIVE US
+                  ANY OF YOUR INFORMATION. IF YOU USE THE SERVICES ON BEHALF OF
+                  SOMEONE ELSE (SUCH AS YOUR CHILD) OR AN ENTITY (SUCH AS YOUR
+                  EMPLOYER), YOU REPRESENT THAT YOU ARE AUTHORISED BY SUCH
+                  INDIVIDUAL OR ENTITY TO (I) ACCEPT THIS PRIVACY POLICY ON SUCH
+                  INDIVIDUAL'S OR ENTITY'S BEHALF, AND (II) CONSENT ON BEHALF OF
+                  SUCH INDIVIDUAL OR ENTITY TO OUR COLLECTION, USE AND
+                  DISCLOSURE OF SUCH INDIVIDUAL'S OR ENTITY'S INFORMATION AS
+                  DESCRIBED IN THIS PRIVACY POLICY.
                 </p>
-                <h4>Lorem Ipsum</h4>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Corporis eius nesciunt et tempora accusamus, eligendi
-                  similique harum expedita cumque magni officiis ipsum iusto hic
-                  deleniti temporibus ab aliquam maiores laudantium? Quisquam
-                  blanditiis aliquam quam voluptatibus fuga voluptates ad
-                  molestiae labore, numquam sapiente cumque saepe ducimus odio
-                  vel quos ex non?
-                </p>
+                <ul>
+                  <li>
+                    <h4>1.WHY THIS PRIVACY POLICY?</h4>
+                    <p>
+                      This Privacy Policy is published in compliance with, inter
+                      alia:
+                    </p>
+                  </li>
+                  <ul>
+                    <li>
+                      {" "}
+                      Section 43A of the Information Technology Act, 2000;
+                    </li>
+                    <li>
+                      Regulation 4 of the Information Technology (Reasonable
+                      Security Practices and Procedures and Sensitive Personal
+                      Information) Rules, 2011 (the "SPI Rules");
+                    </li>
+                    <li>
+                      {" "}
+                      Regulation 3(1) of the Information Technology
+                      (Intermediaries Guidelines) Rules, 2011.
+                      <br />
+                      This Privacy Policy states the following:
+                    </li>
+                    <li>
+                      The type of information collected from the Users,
+                      including Personal Information (as defined in paragraph 2
+                      below) and Sensitive Personal Data or Information (as
+                      defined in paragraph 2 below) relating to an individual;
+                    </li>
+                    <li>
+                      {" "}
+                      The purpose, means and modes of collection, usage,
+                      processing, retention and destruction of such information;
+                      and
+                    </li>
+                    <li>
+                      {" "}
+                      How and to whom ClassHealth will disclose such
+                      information.
+                    </li>
+                  </ul>
+                  <li>
+                    <h4>COLLECTION OF PERSONAL INFORMATION</h4>
+                    <p>
+                      Generally some of the Services require us to know who you
+                      are so that we can best meet your needs. When you access
+                      the Services, or through any interaction with us via
+                      emails, telephone calls or other correspondence, we may
+                      ask you to voluntarily provide us with certain information
+                      that personally identifies you or could be used to
+                      personally identify you. You hereby consent to the
+                      collection of such information by ClassHealth. Without
+                      prejudice to the generality of the above, information
+                      collected by us from you may include (but is not limited
+                      to) the following:
+                      <br />
+                      contact data (such as your email address and phone
+                      number);
+                    </p>
+                  </li>
+                  <ul>
+                    <li>
+                      {" "}
+                      demographic data (such as your gender, your date of birth
+                      and your pin code);
+                    </li>
+                    <li>
+                      data regarding your usage of the services and history of
+                      the appointments made by or with you through the use of
+                      Services;
+                    </li>
+                    <li>
+                      insurance data (such as your insurance carrier and
+                      insurance plan);
+                    </li>
+                    <li>
+                      other information that you voluntarily choose to provide
+                      to us (such as information shared by you with us through
+                      emails or letters.
+                      <br />
+                      The information collected from you by ClassHealth may
+                      constitute 'personal information' or 'sensitive personal
+                      data or information' under the SPI Rules.
+                      <br />
+                      "Personal Information" is defined under the SPI Rules to
+                      mean any information that relates to a natural person,
+                      which, either directly or indirectly, in combination with
+                      other information available or likely to be available to a
+                      body corporate, is capable of identifying such person. The
+                      SPI Rules further define "Sensitive Personal Data or
+                      Information" of a person to mean personal information
+                      about that person relating to:
+                    </li>
+                    <li>passwords;</li>
+                    <li>
+                      financial information such as bank accounts, credit and
+                      debit card details or other payment instrument details;
+                    </li>
+                    <li>
+                      {" "}
+                      physical, physiological and mental health condition;
+                    </li>
+                    <li>sexual orientation;</li>
+                    <li>medical records and history;</li>
+                    <li>biometric information;</li>
+                    <li>
+                      information received by body corporate under lawful
+                      contract or otherwise;
+                    </li>
+                    <li>
+                      visitor details as provided at the time of registration or
+                      thereafter; and
+                    </li>
+                    <li>
+                      call data records. <br />
+                      ClassHealth will be free to use, collect and disclose
+                      information that is freely available in the public domain
+                      without your consent.
+                    </li>
+                  </ul>
+                  <li>
+                    <h4>PRIVACY STATEMENTS</h4>
+                  </li>
+                  <ul>
+                    <li>
+                      ALL USERS NOTE: <br />
+                      This section applies to all users.
+                    </li>
+                    <ul>
+                      <li>
+                        Accordingly, a condition of each User's use of and
+                        access to the Services is their acceptance of the Terms
+                        of Use, which also involves acceptance of the terms of
+                        this Privacy Policy. Any User that does not agree with
+                        any provisions of the same has the option to discontinue
+                        the Services provided by ClassHealth immediately.
+                      </li>
+                      <li>
+                        An indicative list of information that ClassHealth may
+                        require you to provide to enable your use of the
+                        Services is provided in the Schedule annexed to this
+                        Privacy Policy.
+                      </li>
+                      <li>
+                        All the information provided to ClassHealth by a User,
+                        including Personal Information or any Sensitive Personal
+                        Data or Information, is voluntary. You understand that
+                        ClassHealth may use certain information of yours, which
+                        has been designated as Personal Information or
+                        'Sensitive Personal Data or Information' under the SPI
+                        Rules, (a) for the purpose of providing you the
+                        Services, (b) for commercial purposes and in an
+                        aggregated or non-personally identifiable form for
+                        research, statistical analysis and business intelligence
+                        purposes, (c) for sale or transfer of such research,
+                        statistical or intelligence data in an aggregated or
+                        non-personally identifiable form to third parties and
+                        affiliates (d) for communication purpose so as to
+                        provide You a better way of booking appointments and for
+                        obtaining feedback in relation to the Practitioners and
+                        their practice, (e) debugging customer support related
+                        issues.. (f) for the purpose of contacting you to
+                        complete any transaction if you do not complete a
+                        transaction after having provided us with your contact
+                        information in the course of completing such steps that
+                        are designed for completion of the transaction.
+                        ClassHealth also reserves the right to use information
+                        provided by or about the End-User for the following
+                        purposes:
+                        <br />
+                        Publishing such information on the Website.
+                      </li>
+                      <ul>
+                        <li>
+                          Contacting End-Users for offering new products or
+                          services.
+                        </li>
+                        <li>
+                          Contacting End-Users for taking product and Service
+                          feedback.
+                        </li>
+                        <li>
+                          Analyzing software usage patterns for improving
+                          product design and utility.
+                        </li>
+                        
+                      </ul>
+                    </ul>
+                  </ul>
+                </ul>
               </div>
             </Col>
           </Row>
